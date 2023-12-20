c8e107b349657421b65ec6266b16a8c9a79a92b6
Kirill Kurko <kirillkurko2609@gmail.com>
2023-12-20 09:47:51 +0300

    Create 20.ts

---
+++ 20.ts
@@ -0,0 +1,107 @@
+type Letters = {
+  A: [
+    '█▀█ ',
+    '█▀█ ',
+    '▀ ▀ ',
+  ],
+  B: [
+    '█▀▄ ',
+    '█▀▄ ',
+    '▀▀  '
+  ],
+  C: [
+    '█▀▀ ',
+    '█ ░░',
+    '▀▀▀ '
+  ],
+  E: [
+    '█▀▀ ',
+    '█▀▀ ',
+    '▀▀▀ '
+  ],
+  H: [
+    '█ █ ',
+    '█▀█ ',
+    '▀ ▀ '
+  ],
+  I: [
+    '█ ',
+    '█ ',
+    '▀ '
+  ],
+  M: [
+    '█▄░▄█ ',
+    '█ ▀ █ ',
+    '▀ ░░▀ '
+  ],
+  N: [
+    '█▄░█ ',
+    '█ ▀█ ',
+    '▀ ░▀ '
+  ],
+  P: [
+    '█▀█ ',
+    '█▀▀ ',
+    '▀ ░░'
+  ],
+  R: [
+    '█▀█ ',
+    '██▀ ',
+    '▀ ▀ '
+  ],
+  S: [
+    '█▀▀ ',
+    '▀▀█ ',
+    '▀▀▀ '
+  ],
+  T: [
+    '▀█▀ ',
+    '░█ ░',
+    '░▀ ░'
+  ],
+  Y: [
+    '█ █ ',
+    '▀█▀ ',
+    '░▀ ░'
+  ],
+  W: [
+    '█ ░░█ ',
+    '█▄▀▄█ ',
+    '▀ ░ ▀ '
+  ],
+  ' ': [
+    '░',
+    '░',
+    '░'
+  ],
+  ':': [
+    '#',
+    '░',
+    '#'
+  ],
+  '*': [
+    '░',
+    '#',
+    '░'
+  ],
+};
+
+type ToAsciiArt<
+  TInput extends string,
+  TRow extends [string, string, string] = ['', '', ''],
+  TList extends Array<string> = []
+> = TInput extends `\n${infer Rest}`
+  ? ToAsciiArt<Rest, ['', '', ''], [...TList, ...TRow]>
+  : TInput extends `${infer First}${infer Rest}`
+    ? Uppercase<First> extends infer key extends keyof Letters
+      ? ToAsciiArt<
+        Rest,
+        [
+          `${TRow[0]}${Letters[key][0]}`,
+          `${TRow[1]}${Letters[key][1]}`,
+          `${TRow[2]}${Letters[key][2]}`
+        ],
+        TList
+      >
+      : never
+    : [...TList, ...TRow];
